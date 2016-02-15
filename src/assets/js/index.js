05b5019e9e6f6b4e8cf3e3dab2dbf57a6eb47d7e
Luke Chesser <luke.chesser@me.com>
2016-02-15 07:08:46 +0000

    Deploying 26864536e8c6fe569a1bc2f42077f5596b805a14 --skip-ci

--- src/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-// Compile css with webpack
-require("../less/main.less");
-
-// Watch markup
-if (process.env.NODE_ENV !== "production") {
-  require("../../html/index.html");
-}
-
-require("./main");
